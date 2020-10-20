3cddead171e542389f245d10f03937e75d39b872
junjun1023 <jjjunpersonal@gmail.com>
2020-10-20 22:01:26 +0800

    Finish Chessboard Corner Detection

---
+++ hw1/gui.py
@@ -0,0 +1,15 @@
+# from PyQt5.QtWidgets import QApplication, QLabel
+#
+#
+#
+# def main():
+#     app = QApplication([])
+#     label = QLabel('Hello Word')
+#     label.show()
+#     app.exec_()
+#
+#
+#
+#
+# if __name__ == '__main__':
+#     main()
