1d331c5d519ee4ccc4a1d32f8cde4304ebb6250a
junjun1023 <44397290+junjun1023@users.noreply.github.com>
2020-11-05 09:40:01 +0800

    Update ignore files

--- hw1/gui.py
+++ hw1/gui.py
@@ -25,7 +25,13 @@ class MainWindow(QMainWindow):
         [transforms.ToTensor(),
          transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
 
-    testset = torchvision.datasets.CIFAR10(root='./data', train=False,
+    trainset = torchvision.datasets.CIFAR10(root='./models/data', train=True,
+                                            download=True, transform=transform)
+
+    trainloader = torch.utils.data.DataLoader(trainset, batch_size=10,
+                                              shuffle=True, num_workers=2)
+
+    testset = torchvision.datasets.CIFAR10(root='./models/data', train=False,
                                            download=True, transform=transform)
     testloader = torch.utils.data.DataLoader(testset, batch_size=10,
                                              shuffle=False, num_workers=2)
