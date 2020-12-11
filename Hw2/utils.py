144443b7ad826b4cead8d1abaeaf44c8fc45e2e5
junjun1023 <jjjunpersonal@gmail.com>
2020-12-11 17:20:02 +0800

    Finish function of Q2.2

--- Hw2/utils.py
+++ Hw2/utils.py
@@ -96,22 +96,107 @@ def optical_flow(path="./Q2_Image/opticalFlow.mp4"):
                 cv2.line(frame, x_top, x_bottom, (0, 0, 255), 1)
                 cv2.line(frame, y_left, y_right, (0, 0, 255), 1)
 
-            # frame_with_keypoints = cv2.drawKeypoints(frame_gray, keypoints, np.array([]), (0, 0, 255),
-            #                                       cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
             cv2.imshow("Keypoints", frame)
             cv2.waitKey(50)
 
         else:
             # The next frame is not ready, so we try to read it again
-            cap.set(cv2.CAP_PROP_POS_FRAMES, pos_frame - 1)
+            # cap.set(cv2.CAP_PROP_POS_FRAMES, pos_frame - 1)
             # It is better to wait for a while for the next frame to be ready
-            cv2.waitKey(1000)
-
-
-        if cap.get(cv2.CAP_PROP_POS_FRAMES) == cap.get(cv2.CAP_PROP_FRAME_COUNT):
-            # If the number of captured frames is equal to the total number of frames,
-            # we stop
+            # cv2.waitKey(1000)
             break
 
+        # if cap.get(cv2.CAP_PROP_POS_FRAMES) == cap.get(cv2.CAP_PROP_FRAME_COUNT):
+            # If the number of captured frames is equal to the total number of frames,
+            # we stop
+            # break
+
+
+def video_tracking(path="./Q2_Image/opticalFlow.mp4"):
+    cap = cv2.VideoCapture(path)
+    # params for ShiTomasi corner detection
+    feature_params = dict(maxCorners=100,
+                          qualityLevel=0.3,
+                          minDistance=7,
+                          blockSize=7)
+    # Parameters for lucas kanade optical flow
+    lk_params = dict(winSize=(15, 15),
+                     maxLevel=2,
+                     criteria=(cv2.TERM_CRITERIA_EPS | cv2.TERM_CRITERIA_COUNT, 10, 0.03))
+
+    p0 = []
+    prev_frame = None
+    mask = None
+    while True:
+        flag, frame = cap.read()
+        if flag:
+
+            if prev_frame is None:
+
+                # Setup SimpleBlobDetector parameters.
+                params = cv2.SimpleBlobDetector_Params()
+
+                # Change thresholds
+                params.minThreshold = 50
+                params.maxThreshold = 200
+
+                # Filter by Circularity
+                params.filterByCircularity = True
+                params.minCircularity = 0.85
+
+                # Create a detector with the parameters
+                detector = cv2.SimpleBlobDetector_create(params)
+
+                detector.empty()
+                keypoints = detector.detect(frame)
+
+                for keypoint in keypoints:
+                    x = keypoint.pt[0]
+                    y = keypoint.pt[1]
+                    p0.append([x, y])
+                p0 = np.array(p0, dtype='float32')
+                print(p0.shape)
+                p0 = p0[:, np.newaxis, :]
+
+
+                prev_frame = frame
+                mask = np.zeros_like(frame)
+                continue
+
+            else:
+                # calculate optical flow
+                p1, st, err = cv2.calcOpticalFlowPyrLK(prev_frame, frame, p0, None, **lk_params)
+                # Select good points
+                good_new = p1[st == 1]
+                good_old = p0[st == 1]
+                # draw the tracks
+                for i, (new, old) in enumerate(zip(good_new, good_old)):
+                    a, b = new.ravel()
+                    c, d = old.ravel()
+                    mask = cv2.line(mask, (a, b), (c, d), (0, 0, 255), 2)
+                    _frame = cv2.circle(prev_frame, (a, b), 5, (0, 0, 255), -1)
+                img = cv2.add(_frame, mask)
+                cv2.imshow('frame', img)
+                k = cv2.waitKey(30) & 0xff
+                if k == 27:
+                    break
+
+                # Now update the previous frame and previous points
+                # p0 = good_new.reshape(-1, 1, 2)
+                prev_frame = frame
+                p0 = p1
+
+        else:
+            # The next frame is not ready, so we try to read it again
+            # cap.set(cv2.CAP_PROP_POS_FRAMES, pos_frame - 1)
+            # It is better to wait for a while for the next frame to be ready
+            # cv2.waitKey(1000)
+            break
+
+        # if cap.get(cv2.CAP_PROP_POS_FRAMES) == cap.get(cv2.CAP_PROP_FRAME_COUNT):
+            # If the number of captured frames is equal to the total number of frames,
+            # we stop
+            # break
+
 # background_subtraction()
-optical_flow()
+video_tracking()
